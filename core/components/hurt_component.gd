7bd56c4dd53476f9e50a61a03d7cb74c1ac2f37b
Michael Klement <mklement@little-things.de>
2024-01-19 22:03:00 +0100

    Changed: Renamed explosion to splatter_effect - Refactored splatter_effect to also handle the sound effect

--- core/components/hurt_component.gd
+++ core/components/hurt_component.gd
@@ -14,7 +14,7 @@ signal death_finished()
 @export var _death_animation_player: AnimationPlayer
 @export var _death_animation_name: StringName = "death"
 @export var _death_sound: AudioStreamPlayer2D
-@export var _death_vfx: GPUParticles2D
+@export var _death_effect: SplatterEffect
 
 var is_dying = false
 
@@ -60,17 +60,17 @@ func _on_died() -> void:
 	if _death_sound:
 		_death_sound.play()
 
-	if _death_vfx:
-		_death_vfx.emitting = true
+	if _death_effect:
+		_death_effect.play()
 		
 	if _death_animation_player:
 		await _death_animation_player.animation_finished
-	
+
 	if _death_sound:
 		await _death_sound.finished
-
-	if _death_vfx:
-		await _death_vfx.finished
+	
+	if _death_effect:
+		await _death_effect.finished
 	
 	death_finished.emit()
 
---
+++ effects/splatter_effect.gd
@@ -0,0 +1,34 @@
+class_name SplatterEffect
+extends Node2D
+
+
+signal finished
+
+
+@export var splatter_color: Color
+@export var splatter_sound: AudioStream
+
+@onready var _particle_effect := $ParticleEffect as GPUParticles2D
+@onready var _sound_effect := $SoundEffect as AudioStreamPlayer2D
+
+
+func _ready() -> void:
+	_particle_effect.self_modulate = splatter_color
+	_sound_effect.stream = splatter_sound
+
+
+func play() -> void:
+	_particle_effect.emitting = true
+	_sound_effect.play()
+
+
+func stop() -> void:
+	_particle_effect.emitting = false
+	_sound_effect.stop()
+
+
+func _all_effects_finished() -> void:
+	await _particle_effect.finished
+	await _sound_effect.finished
+	
+	finished.emit()
